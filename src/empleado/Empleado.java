4b05764924057ab27ce71a2671bee2119663336b
503 <503@academicos.cesde.edu.co>
2021-09-17 08:35:11 -0500

    se cre clase empleado

---
+++ src/empleado/Empleado.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empleado;
+
+/**
+ *
+ * @author 503
+ */
+public class Empleado {
+    
+    //variables
+    private int id;
+    private String nombre;
+    private String cargo;
+    private double salario;
+    private String telefono;
+    private String direccion;
+    
+    //constructores
+    public Empleado() {
+    }
+
+    public Empleado(int id, String nombre, String cargo, double salario, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.telefono = telefono;
+        this.direccion= direccion;
+    }
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        if(salario<0){
+            this.salario = 0;
+        }else{
+        this.salario = salario;
+        }
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    //fin de getters and setters
+    
+    public double calcularSalario(){
+        return((this.salario-this.salario*0.15)*12);
+    }
+}
